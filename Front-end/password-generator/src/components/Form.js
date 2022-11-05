0f3b0a2287541d28a5ac21be3b4700dcb0d208f5
Teodorof Mihaela <mihaela.teodorof.bd@gmail.com>
2022-11-05 12:27:25 +0200

    Added login functionality and error alerts

--- Front-end/password-generator/src/components/Form.js
+++ Front-end/password-generator/src/components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
@@ -13,6 +13,7 @@ import PasswordIcon from '@mui/icons-material/Password';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import { Card } from '@mui/material';
 import { Typography } from '@mui/material';
+import Alert from '@mui/material/Alert';
 import AutorenewIcon from '@mui/icons-material/Autorenew';
 import LoginIcon from '@mui/icons-material/Login';
 import PixIcon from '@mui/icons-material/Pix';
@@ -20,53 +21,106 @@ import "./Form.css";
 
 function Form() {
 
-    //   async componentDidMount() {
-    //     const response = await fetch('/password');
-    //     const body = await response.json();
-    //     this.setState({clients: body});
-    //   } 
+    const [passwordChange, setPasswordChange] = useState({
+        initialPassword: "Your Password will be here"
+    });
+
+    const [showPassword, setShowPassword] = useState(false);
+    
+    const [passwordInput, setPasswordInput] = useState();
+    const [userIdInput, setUserIdInput] = useState();
+
+    const [showAlert, setShowAlert] = React.useState(false);
+    const [showLoginAlert, setShowLoginAlert] = React.useState(false);
 
     var password;
+    var messagePassword = " Wait 30 second for a new generated password!";
+    var messageLoginFailed = "Your Login was unsuccesful! Try again";
+    var messageLoginSuccessful = "Your Login was succesful!";
 
     async function generatePassword() {
 
+        try {
+            const baseURL = "http://localhost:5232";
 
-        //const [textInput] = useState('');
+            const encodedValue = encodeURIComponent(userIdInput);
+            const res = await fetch(`${baseURL}/password?userId=${encodedValue}`, {
+                method: "GET",
+                datatype: "json"
+            });
+            console.log(res);
 
-        const baseURL = "http://localhost:5232";
+            if (res.status === 204) {
+                setShowAlert(true);
+                const message = `Wait 30s for a new password, response: ${res.status} - ${res.statusText}`;
+                throw new Error(message);
+            } else {
+                setShowAlert(false);
+            }
 
-        const encodedValue = encodeURIComponent("08dabbe0-a463-408f-8778-26e2ace162b1");
-        const res = await fetch(`${baseURL}/password?userId=${encodedValue}`, {
-            method: "GET",
-            datatype: "json"
-        });
-        //.then((response) => console.log(response));
+            const data = await res.json();
 
 
-        const data = await res.json();
+            const result = {
+                data: data,
+                status: res.status,
+                statusText: res.statusText,
+            };
+            console.log(result.statusText);
+            password = data['password'];
+            console.log(password);
 
-        const result = {
-            data: data,
-            status: res.status,
-            statusText: res.statusText,
-        };
-        console.log(result.statusText);
-        password = data['password'];
-        console.log(password);
+            changeInitialPassword();
 
-        changeInitialPassword();
 
+        }
+        catch (err) {
+            console.log(err);
+        }
     }
 
+    async function login() {
+        try {
+            const baseURL = "http://localhost:5232";
+
+            const res = await fetch(`${baseURL}/login`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "post",
+                body: JSON.stringify({
+                    "UserId": userIdInput,
+                    "Password": passwordInput
+                })
+            });
+
+            const data = await res.json();
+
+            console.log(data);
+
+            if (data == true) {
+                setShowLoginAlert(true);
+                const message = `Login successful, response: ${res.status} - ${res.statusText}`;
+                console.log(message);
+            }
+            else {
+                setShowLoginAlert(false);
+                const message = `Login failed, response: ${res.status} - ${res.statusText}`;
+                console.log(message);
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
     //   useEffect(() => {
     //     fetch(`/password`)
     //      .then((response) => console.log(response));
     //    }, []);
 
     //show generated password 
-    const [passwordChange, setPasswordChange] = useState({
-        initialPassword: "Your Password will be here"
-    });
+
 
     function changeInitialPassword() {
         setPasswordChange({
@@ -75,10 +129,6 @@ function Form() {
         });
     }
 
-    const [showPassword, setShowPassword] = useState(false);
-
-    const [passwordInput, setPasswordInput] = useState();
-    const [userIdInput, setUserIdInput] = useState();
 
     function onReset() {
         setPasswordInput("");
@@ -117,8 +167,10 @@ function Form() {
                                     }}>
                                 </TextField>
                             </div>
-                            <div>
-                                <Button>Login <LoginIcon /></Button>
+                            <div>{showLoginAlert ? <Alert severity="success">
+                                {messageLoginSuccessful}</Alert>
+                                : <Alert severity="error">{messageLoginFailed} </Alert>}
+                                <Button onClick={login}>Login <LoginIcon /></Button>
                                 <Button onClick={onReset}>Reset <AutorenewIcon /></Button>
                             </div>
                         </Item>
@@ -126,6 +178,8 @@ function Form() {
                             <Card>
                                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom> Your one time password is:
                                 </Typography>
+                                {showAlert ? <Alert severity="error">
+                                    {messagePassword}</Alert> : null}
                                 <Typography variant="h5" component="div" >
                                     {passwordChange.initialPassword}
                                 </Typography>
